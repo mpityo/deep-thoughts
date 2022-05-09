69c65cda811ea240d4345404386906e3427654d0
Matt Pityo <mpityo@gmail.com>
2022-05-09 16:31:32 -0400

    reaction and thought typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
+// import apollo and handlers
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+// connect to database
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+// create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// create new instance of an Apollo server with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+};
+
+// call the async function to start the server
+startApolloServer(typeDefs, resolvers);
