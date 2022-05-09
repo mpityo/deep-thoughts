578802821781c499d68dbd455e59ecb4ef06992f
Matt Pityo <mpityo@gmail.com>
2022-05-09 19:52:24 -0400

    User queries and resolvers, single and multiple queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,29 @@ const { User, Thought } = require('../models');
 const resolvers = {
     Query: {
         // parent is just a placeholder so we can access the second parameter
+        // get all thoughts
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
+        },
+        // get single thought by id
+        thought: async(parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get user by username
+        user: async(parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         }
     }
 };
